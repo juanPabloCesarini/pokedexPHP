e0220be27f3fd83553c7c63152816e71b150e168
Fran <francisco.chardin@gmail.com>
2021-03-02 20:06:06 -0300

    primeros pasos vista

--- models/pokedex_model.php
+++ models/pokedex_model.php
@@ -1,5 +1,6 @@
 <?php
-
+/* Acá debería leer el input con la barra de busqueda en cada caracter que se tipée
+creo que debería consultar la api a la vez para hacer el autocompletado */
 $pokemon = "pikachu";
 $url = "https://pokeapi.co/api/v2/pokemon/".$pokemon;
 $api = curl_init($url);
--- views/pokedex_view.php
+++ views/pokedex_view.php
@@ -1,20 +1,49 @@
+
 <?php
-
 include("././controllers/pokedex_controller.php");
+?>
 
-echo "<h2>HOLA POKE USERS</h2>";
-
-echo "<h3>Habilidades</h3>";
-  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pokemon Api</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=DotGothic16&family=Source+Sans+Pro&display=swap" rel="stylesheet">
+  <link rel="shortcut icon" href="./img/favicon.ico">
+  <link rel="stylesheet" href="./views/css/normalize.css">
+  <link rel="stylesheet" href="./views/css/style.css">
+</head>
+<body>
+<header>
+  <span>
+    <h1>Pokemon - pokedex Api</h1>
+    <p>Connected to: <a href="https://pokeapi.co/" target="_blank"><img class="img-header" src="./img/pokeapi.png" alt="pokeapi-logo"></a></p>
+  </span>
+</header>
+<section class="search-bar">
+    <input type="text" placeholder="Type in pokémon name"></input>
+</section>
+<h2>Habilidades</h2>
+<?php 
 foreach ($json->abilities as $ab) {
-    echo $ab->ability->name."<br>";
-  }
-  
-echo "<h2>TIPO</h2>";
+  echo $ab->ability->name."<br>";
+}
+?> 
+<?php
+  echo 
+  '<div>
+    <img src="'.$json->sprites->front_default.'"width="200">
+    <img src="'.$json->sprites->back_default.'"width="200">
+  </div>'; 
+?>
+<?php
+echo "<h2>Type</h2>";
 
 echo $json->types[0]->type->name;
-echo "<h2>FOTOS</h2>";
-echo '<img src="'.$json->sprites->front_default.'"width="200">'; 
-echo '<img src="'.$json->sprites->back_default.'"width="200">';
 
 ?>
+</body>
+</html>
